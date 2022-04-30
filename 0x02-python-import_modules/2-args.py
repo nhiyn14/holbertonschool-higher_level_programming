ab0fbabed5b106c43a43ac137ea5e9c3c5772f21
Cienna Nguyen <4326@holbertonschool.com>
2022-04-30 15:02:00 +1000

    Fix print with differnt argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
         print("0 arguments.")
-    else:
-        print("{} arguments:".format(len(argv)-1))
+    elif len(argv) >= 2:
+        if len(argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(argv)-1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
