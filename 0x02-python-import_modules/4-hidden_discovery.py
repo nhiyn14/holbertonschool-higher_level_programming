38645f2755b473b3b00b43013ee0ca02f7cfb14f
Cienna Nguyen <4326@holbertonschool.com>
2022-04-30 15:18:33 +1000

    Add 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    name = dir(hidden_4)
+
+    for i in range(len(name)):
+        if (name[i][0] != '_' and name[i][1] != '_'):
+            print("{}".format(name[i]))
