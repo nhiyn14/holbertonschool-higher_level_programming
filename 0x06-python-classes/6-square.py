d05333902892250f2ada159e4891d4fa9fa0be04
Cienna Nguyen <4326@holbertonschool.com>
2022-05-17 17:23:14 +1000

    Add file

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Create a class with Public instance methods"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size (int): Square size
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Property to retrieve __size
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for __size
+
+        Args:
+            value (int): Square size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        """Make value Private instance attribute size
+
+        Args:
+            __size (int): Square size
+        """
+        self.__size = value
+
+    @property
+    def position(self):
+        """Property to retrieve __position
+
+        Return: position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for __position
+
+        Args:
+            value (int): Square position
+        """
+        if type(value) is not tuple:
+            if value[0] < 0 or value[1] < 0:
+                raise TypeError("position must be a tuple of 2 positive integers")
+
+        """Make value Private instance attribute size
+
+        Args:
+            __size (int): Square size
+        """
+        self.__position = value
+
+    def area(self):
+        """Public instance method 'area'
+
+        Return: current square area
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ prints in stdout the square with the character #
+
+        Return: Square of #
+        """
+        if self.__size != 0:
+            for row in range(self.size):
+                for col in range(self.position[0]):
+                    print(" ", end="")
+                for col in range(self.size):
+                    print("#", end="")
+                print()
+            print()
+        else:
+            print()
