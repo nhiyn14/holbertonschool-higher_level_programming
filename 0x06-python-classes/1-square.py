ae5a680672a10c3b0244100d33077c64076e9ebf
Cienna Nguyen <4326@holbertonschool.com>
2022-05-17 15:51:39 +1000

    Add documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""Create a class with a Private instance attribute"""
+
+
 class Square:
+    """Defines a square
+
+    Attributes:
+        __size (int): Square size
+    """
     def __init__(self, size):
+        """Make size Private instance attribute
+
+        Args:
+            size (int): Square size
+
+        Return: None
+        """
         self.__size = size
