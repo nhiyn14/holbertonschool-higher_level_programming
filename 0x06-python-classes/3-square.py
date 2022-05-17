d3428aa22e43ead843c8426033929b48a4c8b867
Cienna Nguyen <4326@holbertonschool.com>
2022-05-17 16:03:48 +1000

    Add documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
+"""Create a class with Public instance method"""
+
+
 class Square:
+    """Defines a square
+
+    Attributes:
+        __size (int): Square size
+    """
     def __init__(self, size=0):
-        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if type(size) is int and size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        """Make size Private instance attribute
+
+        Args:
+            size (int): Square size
+
+        Return: None
+        """
+        self.__size = size
 
     def area(self):
+        """Public instance method 'area'
+
+        Return: current square area
+        """
         return self.__size * self.__size
