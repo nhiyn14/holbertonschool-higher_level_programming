8057e1cc92ebf337bf120e06acea2026a67c1795
Cienna Nguyen <4326@holbertonschool.com>
2022-05-23 22:24:10 +1000

    Add 4-print_square module and test file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_square(size):
+    """a square with the character #
+    Returns: square of #
+    """
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if size > 0:
+        for i in range(size):
+            print("#" * size + "\n", end="")
