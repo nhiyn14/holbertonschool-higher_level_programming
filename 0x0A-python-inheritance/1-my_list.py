15cb593318c01d098fc351c26d79a50e6d952b25
Cienna Nguyen <4326@holbertonschool.com>
2022-05-30 20:35:17 +1000

    Fix documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,7 @@ class MyList(list):
     def __init__(self):
         """Create instance"""
         super().__init__()
+
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
         print(sorted(self))
