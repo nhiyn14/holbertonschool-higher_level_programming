079cdc475fbbcf5b47020cf145f161ed268d2fae
Cienna Nguyen <4326@holbertonschool.com>
2022-05-30 21:04:13 +1000

    Add file

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a subclass of a specific class
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
