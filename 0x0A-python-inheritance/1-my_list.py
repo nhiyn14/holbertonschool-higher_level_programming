338ff4e17f1ddc881af18bf2b9a79e1d364537d4
Cienna Nguyen <4326@holbertonschool.com>
2022-05-30 20:34:12 +1000

    Add 1-my_list module and test file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """subclass of list"""
+    def __init__(self):
+        """Create instance"""
+        super().__init__()
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
