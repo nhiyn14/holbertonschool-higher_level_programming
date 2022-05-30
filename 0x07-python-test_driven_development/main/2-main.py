87c407c63653ca40fa679935c9b79d2f6ff9973a
Cienna Nguyen <4326@holbertonschool.com>
2022-05-30 23:20:27 +1000

    Add main folder

---
+++ 0x07-python-test_driven_development/main/2-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 3, float('inf')],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 2))
+print(matrix)
+
---
+++ 0x0A-python-inheritance/main/1-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList(4)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
