03e46abf971e4ad5218f99741e44521e94a71c74
Cienna Nguyen <4326@holbertonschool.com>
2022-05-31 18:14:32 +1000

    Add file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student
+"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Create instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) is not list or attrs is None:
+            return self.__dict__
+        newDict = {}
+        for key in attrs:
+            try:
+                if self.__dict__[key]:
+                    newDict[key] = self.__dict__[key]
+            except Exception:
+                pass
+        return newDict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance with json dict"""
+        for key in json:
+            setattr(self, key, json[key])
