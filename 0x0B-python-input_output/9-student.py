534c99d3085d40895a1897eeab0f8ad8facee02a
Cienna Nguyen <4326@holbertonschool.com>
2022-05-31 17:59:26 +1000

    Add file

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student
+"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Create instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
