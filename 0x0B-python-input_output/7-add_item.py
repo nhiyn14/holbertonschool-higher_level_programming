aa27149e41dc974f0cece38639bd000106bc71cd
Cienna Nguyen <4326@holbertonschool.com>
2022-05-31 17:49:37 +1000

    Fix pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     list = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     list = []
 
 argList = sys.argv[1:]
