c57fee33c9e927bb60148aa4b03b5528a7684efc
Cienna Nguyen <4326@holbertonschool.com>
2022-06-04 12:43:08 +1000

    Correct the setter for width and height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,8 +26,8 @@ class Rectangle(Base):
         """setter property for __width to value"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -40,8 +40,8 @@ class Rectangle(Base):
         """setter property for __height to value"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
