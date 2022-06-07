db219a036e69c0d41bf2344313873cc8e7e1b847
Cienna Nguyen <4326@holbertonschool.com>
2022-06-07 15:02:36 +1000

    Fix the correct content for test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,71 +1,189 @@
 #!/usr/bin/python3
 """
-Class Base
+Contains tests for Base class
 """
 
-
+import unittest
+import inspect
+import pycodestyle
 import json
+import os
+
+from models.rectangle import Rectangle
+from models.square import Square
+from models import base
+Base = base.Base
 
 
-class Base:
-    """Class Base"""
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """Init an instance"""
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = self.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """returns JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+class TestBaseDocs(unittest.TestCase):
+    """ Tests for documentation of class"""
 
     @classmethod
-    def save_to_file(cls, list_objs):
-        """writes JSON string representation of list_objs to a file"""
-        fileName = cls.__name__ + ".json"
-        newFile = []
-        if list_objs is not None:
-            for i in list_objs:
-                newFile.append(cls.to_dictionary(i))
-        with open(fileName, 'w', encoding="utf-8") as w:
-            w.write(cls.to_json_string(newFile))
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
 
-    @staticmethod
-    def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
-            return []
-        else:
-            return json.loads(json_string)
+    def test_conformance_class(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
+    def test_conformance_test(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstr(self):
+        """ Tests for docstring"""
+        self.assertTrue(len(base.__doc__) >= 1)
+
+    def test_class_docstr(self):
+        """ Tests for docstring"""
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstr(self):
+        """Tests for docstrings in all functions"""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
+
+class TestBase(unittest.TestCase):
+    """ Tests functionality of class"""
     @classmethod
-    def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
-        if cls.__name__ == "Rectangle":
-            instance = cls(1, 1)
-        if cls.__name__ == "Square":
-            instance = cls(1)
-        instance.update(**dictionary)
-        return instance
+    def setUpClass(cls):
+        """ set up all r_ attributes for all tests """
+        Base._Base__nb_objects = 0
+        cls.b_2 = Square(8, 0, 0, 8)
+        cls.b_3 = Square(3)
 
-    @classmethod
-    def load_from_file(cls):
-        """returns a list of instances"""
-        fileName = cls.__name__ + ".json"
-        liSt = []
+    def test_id_none(self):
+        """ Tests id as none"""
+        Base._Base__nb_objects = 0
+        b_1 = Square(5)
+        self.assertEqual(b_1.id, 1)
+
+    def test_id_assigned(self):
+        """ Tests id is give"""
+        self.assertEqual(self.b_2.id, 8)
+
+    def test_too_many_args(self):
+        """ Tests entering too many args to instantiate class"""
+        with self.assertRaises(TypeError):
+            b = Base(1, 2)
+
+    def test_nb_attribute(self):
+        """Tests nb_objects as a private instance attribute"""
+        with self.assertRaises(AttributeError):
+            print(self.b_3.nb_objects)
+        with self.assertRaises(AttributeError):
+            print(self.b_3.__nb_objects)
+
+    def test_to_json_string(self):
+        """Tests to json string"""
+        Base._Base__nb_objects = 0
+        dictTest1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
+        dictTest2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
+        jsonStr = Base.to_json_string([dictTest1, dictTest2])
+        self.assertTrue(type(jsonStr) is str)
+        newDict = json.loads(jsonStr)
+        self.assertEqual(newDict, [dictTest1, dictTest2])
+
+    def test_empty_to_json_string(self):
+        """Test for passing empty list/ None"""
+        jsonStr = Base.to_json_string([])
+        self.assertTrue(type(jsonStr) is str)
+        self.assertEqual(jsonStr, "[]")
+
+    def test_None_to_json_String(self):
+        """test for None to JSON converter"""
+        jsonStr = Base.to_json_string(None)
+        self.assertTrue(type(jsonStr) is str)
+        self.assertEqual(jsonStr, "[]")
+
+    def test_write_file_square(self):
+        """tests write to file square"""
+        s1 = Square(1, 1, 1, 1)
+        s2 = Square(2, 2, 2, 2)
+        Square.save_to_file([s1, s2])
+        with open('Square.json', 'r', encoding='utf-8') as f:
+            text = f.read()
+        textAsDicts = eval(text)
+        self.assertEqual(textAsDicts[0]['id'], 1)
+        self.assertEqual(textAsDicts[1]['x'], 2)
+
+    def test_write_file_rectangle(self):
+        """tests write to file for rectangle"""
+        r1 = Rectangle(3, 3, 3, 3, 3)
+        r2 = Rectangle(4, 4, 4, 4, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open('Rectangle.json', 'r', encoding='utf-8') as f:
+            text = f.read()
+        textAsDicts = eval(text)
+        self.assertEqual(textAsDicts[0]['id'], 3)
+        self.assertEqual(textAsDicts[1]['x'], 4)
+
+#    def test_write_file_rectangle_with_junk(self):
+#        """tests write to file with just information"""
+#        r1 = Rectangle(3, 3, 3, 3, 3)
+#        r2 = Rectangle(4, 4, 4, 4, 4)
+#        junk = {"id": 123, "width": 5, "height": 5, "x": 2, "y": 4}
+#        Rectangle.save_to_file([junk, r1, r2])
+#        with open('Rectangle.json', 'r', encoding='utf-8') as f:
+#            text = f.read()
+#        textAsDicts = eval(text)
+#        self.assertEqual(textAsDicts[0]['id'], 3)
+#        self.assertEqual(textAsDicts[1]['x'], 4)
+
+    def test_empty_from_JSON_str(self):
+        """Tests for None from JSON converter"""
+        self.assertEqual([], Base.from_json_string(""))
+
+    def test_None_from_JSON_str(self):
+        """Test for passing empty string"""
+        self.assertEqual([], Base.from_json_string(None))
+
+    def test_create_square(self):
+        """tesrt create method for square class"""
+        s = Square(1, 2, 3, 4)
+        sqDict = s.to_dictionary()
+        s2 = Square.create(**sqDict)
+        self.assertFalse(s is s2)
+        self.assertEqual(s.id, s2.id)
+        self.assertEqual(s.y, s2.y)
+        self.assertEqual(s.x, s2.x)
+        self.assertEqual(s.size, s2.size)
+
+    def test_create_rectangle(self):
+        """test create method for rectangle"""
+        r = Rectangle(1, 2, 3, 4, 5)
+        reDict = r.to_dictionary()
+        r2 = Rectangle.create(**reDict)
+        self.assertEqual(r.id, r2.id)
+        self.assertEqual(r.y, r2.y)
+        self.assertEqual(r.x, r2.x)
+        self.assertEqual(r.height, r2.height)
+        self.assertEqual(r.width, r2.width)
+
+    def test_read_from_file(self):
+        """tests the read from file method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(list_rectangles_output[0].y, 8)
+        self.assertEqual(list_rectangles_output[1].height, 4)
+
+    def test_empty_read_from_file(self):
+        """tests the read from file method with empty file"""
         try:
-            with open(fileName, 'r') as r:
-                liSt = cls.from_json_string(r.read())
-            for i, inst in enumerate(liSt):
-                liSt[i] = cls.create(**liSt[i])
+            os.remove('Square.json')
         except Exception:
             pass
-        return liSt
+        list_output = Square.load_from_file()
+        self.assertEqual(len(list_output), 0)
+        self.assertEqual(list, type(list_output))
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,126 +1,359 @@
 #!/usr/bin/python3
 """
-Class Rectangle that inherits from Base
+Contains tests for Rectangle class - Task 2
+Focus is on:
+- setting private instance attributes in the constructor using
+  getters and setters
+- calling super()__init__() to set public instance id attribute
 """
 
+import unittest
+import inspect
+import pycodestyle
+
 from models.base import Base
+from models import rectangle
+Rectangle = rectangle.Rectangle
 
 
-class Rectangle(Base):
-    """Class Rectangle that inherits from Base"""
+class TestRectangleDocs(unittest.TestCase):
+    """Tests for documentation of class"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Init an instance"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        super().__init__(id)
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.rect_funcs = inspect.getmembers(Rectangle, inspect.isfunction)
 
-    @property
-    def width(self):
-        """getter property to retrieve __width"""
-        return self.__width
+    def test_conformance_class(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    @width.setter
-    def width(self, value):
-        """setter property for __width to value"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+    def test_conformance_test(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_models/test_rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    @property
-    def height(self):
-        """getter property to retrieve __height"""
-        return self.__height
+    def test_module_docstr(self):
+        """Tests for docstring"""
+        self.assertTrue(len(rectangle.__doc__) >= 1)
 
-    @height.setter
-    def height(self, value):
-        """setter property for __height to value"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
+    def test_class_docstr(self):
+        """Tests for docstring"""
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
 
-    @property
-    def x(self):
-        """getter property to retrieve __x"""
-        return self.__x
+    def test_func_docstr(self):
+        """Tests for docstrings in all functions"""
+        for func in self.rect_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
 
-    @x.setter
-    def x(self, value):
-        """setter property for __x to value"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
 
-    @property
-    def y(self):
-        """getter property to retrieve __y"""
-        return self.__y
+class TestRectangle(unittest.TestCase):
+    """ Tests functionality of class"""
 
-    @y.setter
-    def y(self, value):
-        """setter property for __y to value"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+    def test_properties(self):
+        """Tests all setters and getters"""
+        Base._Base__nb_objects = 0
+        r_1 = Rectangle(1, 1)
+        self.assertEqual(r_1.width, 1)
+        self.assertEqual(r_1.height, 1)
+        self.assertEqual(r_1.x, 0)
+        self.assertEqual(r_1.y, 0)
 
-    def area(self):
-        """returns the Rectangle area"""
-        return self.__height * self.__width
+        r_1.x = 10
+        r_1.y = 10
+        self.assertEqual(r_1.x, 10)
+        self.assertEqual(r_1.y, 10)
 
-    def display(self):
-        """prints in stdout the Rectangle instance of #"""
-        print('\n' * self.__y, end="")
-        for i in range(self.__height):
-            print(' ' * self.__x, end="")
-            print('#' * self.__width)
+    """ Tests functionality of super()__init__() call"""
 
-    def __str__(self):
-        """return [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
-                f" - {self.__width}/{self.__height}")
+    def test_id_none(self):
+        """Tests id not passed in"""
+        Base.__nb_object = 0
+        r_2 = Rectangle(1, 1)
+        self.assertEqual(r_2.id, 1)
 
-    def update(self, *args, **kwargs):
-        """assigns a key/value argument to each attribute"""
-        if len(args) > 0:
-            for i, arg in enumerate(args):
-                if i == 0:
-                    self.id = arg
-                if i == 1:
-                    self.__width = arg
-                if i == 2:
-                    self.__height = arg
-                if i == 3:
-                    self.__x = arg
-                if i == 4:
-                    self.__y = arg
-        else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "width" in kwargs:
-                self.__width = kwargs["width"]
-            if "height" in kwargs:
-                self.__height = kwargs["height"]
-            if "x" in kwargs:
-                self.__x = kwargs["x"]
-            if "y" in kwargs:
-                self.__y = kwargs["y"]
+    def test_id_assigned(self):
+        """Tests id passed in"""
+        r_3 = Rectangle(1, 1, 1, 1, 88)
+        self.assertEqual(r_3.id, 88)
 
-    def to_dictionary(self):
-        """returns the dictionary representation of a Rectangle"""
-        dictRec = {}
-        dictRec["id"] = self.id
-        dictRec["width"] = self.width
-        dictRec["height"] = self.height
-        dictRec["x"] = self.x
-        dictRec["y"] = self.y
-        return dictRec
+    def test_nb_object_increment(self):
+        """Tests private class attribute incrementation"""
+        r_4 = Rectangle(1, 1)
+        self.assertEqual(r_4.id, 2)
+
+    """Tests number of arguments passed in"""
+    def test_too_many_args(self):
+        """Tests entering too many args to instantiate object"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(1, 2, 3, 4, 5, 6)
+
+    def test_too_few_args(self):
+        """Tests entering too few args to instantiate object"""
+        with self.assertRaises(TypeError):
+            r = Rectangle()
+
+
+class TestRectangle_w(unittest.TestCase):
+    """ Unit tests for Rectangle width attribute """
+
+    def test_neg_w(self):
+        """ Test negative number """
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(-3, 5)
+
+    def test_zero_w(self):
+        """ Test 0 """
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 5)
+
+    def test_float_w(self):
+        """ Test float """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(1.2, 5)
+
+    def test_tuple_w(self):
+        """ Test a tuple """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle((3, 2), 5)
+
+    def test_infinity_w(self):
+        """ Test infinity """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(float('inf'), 3)
+
+    def test_nan_w(self):
+        """ Test NaN """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(float('nan'), 3)
+
+    def test_string_w(self):
+        """ Test string """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle("hello", 5)
+
+    def test_list_w(self):
+        """ Test list """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle([1, 2], 5)
+
+    def test_dict_w(self):
+        """ Test dictionary """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle({"1": 1, "2": 2}, 5)
+
+
+class TestRectangle_h(unittest.TestCase):
+    """ Unit tests for Rectangle height attribute """
+
+    def test_neg_h(self):
+        """ Test negative number """
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(3, -5)
+
+    def test_zero_h(self):
+        """ Test 0 """
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(5, 0)
+
+    def test_float_h(self):
+        """ Test float """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(1, 5.1)
+
+    def test_tuple_h(self):
+        """ Test a tuple """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(5, (3, 2))
+
+    def test_infinity_h(self):
+        """ Test infinity """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(3, float('inf'))
+
+    def test_nan_h(self):
+        """ Test NaN """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(3, float('nan'))
+
+    def test_string_h(self):
+        """ Test string """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(5, "hello")
+
+    def test_list_h(self):
+        """ Test list """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(5, [1, 2])
+
+    def test_dict_h(self):
+        """ Test dictionary """
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(5, {"1": 1, "2": 2})
+
+
+class TestRectangle_x(unittest.TestCase):
+    """ Unit tests for Rectangle x attribute """
+
+    def test_neg_x(self):
+        """ Test negative number """
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Rectangle(3, 5, -2)
+
+    def test_float_x(self):
+        """ Test float """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(1, 3, 5.1)
+
+    def test_tuple_x(self):
+        """ Test a tuple """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(5, 4, (3, 2))
+
+    def test_infinity_x(self):
+        """ Test infinity """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(3, 4, float('inf'))
+
+    def test_nan_x(self):
+        """ Test NaN """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(3, 4, float('nan'))
+
+    def test_string_x(self):
+        """ Test string """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(5, 4, "hello")
+
+    def test_list_x(self):
+        """ Test list """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(5, 4, [1, 2])
+
+    def test_dict_x(self):
+        """ Test dictionary """
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(5, 4, {"1": 1, "2": 2})
+
+
+class TestRectangle_y(unittest.TestCase):
+    """ Unit tests for Rectangle y attribute """
+
+    def test_neg_y(self):
+        """ Test negative number """
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Rectangle(3, 5, 6, -2)
+
+    def test_float_y(self):
+        """ Test float """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(1, 3, 4, 5.1)
+
+    def test_tuple_y(self):
+        """ Test a tuple """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(5, 4, 6, (3, 2))
+
+    def test_infinity_y(self):
+        """ Test infinity """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(3, 4, 5, float('inf'))
+
+    def test_nan_y(self):
+        """ Test NaN """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(3, 4, 5, float('nan'))
+
+    def test_string_y(self):
+        """ Test string """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(5, 4, 3, "hello")
+
+    def test_list_y(self):
+        """ Test list """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(5, 4, 3, [1, 2])
+
+    def test_dict_y(self):
+        """ Test dictionary """
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(5, 4, 3, {"1": 1, "2": 2})
+
+
+class TestRectangle_area(unittest.TestCase):
+    """ Unit tests for area method """
+
+    def test_area_(self):
+        """ Test area """
+        rec = Rectangle(5, 20, 1, 1, 0)
+        self.assertEqual(100, rec.area())
+
+    def test_area_large(self):
+        """ Test large area """
+        rec = Rectangle(35468786453513213654654, 3165488674541351, 1, 1, 0)
+        self.assertEqual(112276041818321768348545216113956597554, rec.area())
+
+    def test_area_change(self):
+        """ Test changed attributes """
+        rec = Rectangle(5, 20, 1, 1, 0)
+        rec.width = 2
+        rec.height = 4
+        self.assertEqual(8, rec.area())
+
+
+class TestRectangle_update(unittest.TestCase):
+    """ Unit tests for update method """
+
+    def test_update_id(self):
+        """ Test update 1st argument """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(10)
+        self.assertEqual("[Rectangle] (10) 3/4 - 1/2", str(rec))
+
+    def test_update_w(self):
+        """ Test update 2nd argument """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(10, 9)
+        self.assertEqual("[Rectangle] (10) 3/4 - 9/2", str(rec))
+
+    def test_update_h(self):
+        """ Test update 3rd argument """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(10, 9, 8)
+        self.assertEqual("[Rectangle] (10) 3/4 - 9/8", str(rec))
+
+    def test_update_x(self):
+        """ Test update 4th argument """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(10, 9, 8, 7)
+        self.assertEqual("[Rectangle] (10) 7/4 - 9/8", str(rec))
+
+    def test_update_y(self):
+        """ Test update 5th argument """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(10, 9, 8, 7, 6)
+        self.assertEqual("[Rectangle] (10) 7/6 - 9/8", str(rec))
+
+    def test_update_again(self):
+        """ Test update twice """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(10, 9, 8, 7, 6)
+        rec.update(11, 12, 13, 14, 15)
+        self.assertEqual("[Rectangle] (11) 14/15 - 12/13", str(rec))
+
+    def test_update_kwargs(self):
+        """ Test updating with key, values """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(y=10, id=9, height=8, width=7, x=6)
+        self.assertEqual("[Rectangle] (9) 6/10 - 7/8", str(rec))
+
+    def test_update_None(self):
+        """ Test None in id """
+        rec = Rectangle(1, 2, 3, 4, 5)
+        rec.update(None, 9, 8, 7, 6)
+        self.assertEqual("[Rectangle] (None) 7/6 - 9/8", str(rec))
