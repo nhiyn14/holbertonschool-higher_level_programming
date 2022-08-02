01e40f0ed59e46f9506ca3f7c6b5c00505eea1d0
Cienna Nguyen <4326@holbertonschool.com>
2022-08-02 22:55:10 +1000

    Add file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// a class Square that defines a square and inherits from Rectangle
+
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+};
