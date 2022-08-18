a10ebba3a4e24ce3bba643db4fcd3d5cf40cd301
Cienna Nguyen <4326@holbertonschool.com>
2022-08-19 00:05:37 +1000

    Add documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
+    """fetch URL"""
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
     print("Body response:")
