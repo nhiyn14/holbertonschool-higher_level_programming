f91ea10146062b4c917ae639ce24e8357219bc34
Cienna Nguyen <4326@holbertonschool.com>
2022-08-19 10:39:09 +1000

    Change fstring to .format

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {type(req.text)}")
-    print(f"\t- content: {req.text}")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
