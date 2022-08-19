b1f3a4df98f7bf18d8ae27da534bf97c2bddfddc
Cienna Nguyen <4326@holbertonschool.com>
2022-08-19 10:52:34 +1000

    Add file

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,6 +9,6 @@ import sys
 
 
 if __name__ == "__main__":
-    req = requests.get(sys.argv[1])
-    if "X-Request-Id" in req.text:
-        print(html['X-Request-Id'])
+    req = requests.head(sys.argv[1])
+    if "X-Request-Id" in req.headers:
+        print(req.headers['X-Request-Id'])
